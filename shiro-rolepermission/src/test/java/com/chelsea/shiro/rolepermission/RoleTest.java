ca2a8c8db122bc6c41b548e1c498ea13629a22d3
412745310@qq.com <412745310@qq.com>
2017-09-24 21:57:09 +0800

    添加shiro-rolepermission模块

---
+++ shiro-rolepermission/src/test/java/com/chelsea/shiro/rolepermission/RoleTest.java
@@ -0,0 +1,44 @@
+package com.chelsea.shiro.rolepermission;
+
+import java.util.Arrays;
+
+import org.apache.shiro.subject.Subject;
+import org.junit.Test;
+
+import com.chelsea.shiro.rolepermission.util.ShiroUtil;
+
+public class RoleTest {
+
+	@Test
+	public void testHasRole() {
+		Subject currentUser = ShiroUtil.login("classpath:role_permission.ini",
+				"java1234", "123456");
+		// Subject currentUser=ShiroUtil.login("classpath:role_permission.ini",
+		// "jack", "123");
+		System.out.println(currentUser.hasRole("role1") ? "有role1这个角色"
+				: "没有role1这个角色");
+		boolean[] results = currentUser.hasRoles(Arrays.asList("role1",
+				"role2", "role3"));
+		System.out.println(results[0] ? "有role1这个角色" : "没有role1这个角色");
+		System.out.println(results[1] ? "有role2这个角色" : "没有role2这个角色");
+		System.out.println(results[2] ? "有role3这个角色" : "没有role3这个角色");
+		System.out.println(currentUser.hasAllRoles(Arrays.asList("role1",
+				"role2")) ? "role1,role2这两个角色都有" : "role1,role2这个两个角色不全有");
+
+		currentUser.logout();
+	}
+
+	@Test
+	public void testCheckRole() {
+		Subject currentUser = ShiroUtil.login("classpath:role_permission.ini",
+				"java1234", "123456");
+		// Subject currentUser=ShiroUtil.login("classpath:shiro_role.ini",
+		// "jack", "123");
+		currentUser.checkRole("role1");
+		currentUser.checkRoles(Arrays.asList("role1", "role2"));
+		currentUser.checkRoles("role1", "role2", "role3");
+
+		currentUser.logout();
+	}
+
+}
